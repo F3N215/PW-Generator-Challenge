c20d987a2efb1a5995c2310f9efb38cef80e899b
Daniel John <dcampo85@gmail.com>
2023-11-30 16:05:39 -0500

    correcting git transfer

--- script.js
+++ script.js
@@ -1,59 +1,84 @@
-// constant variables for password text
+/* standard password constant variables for character sets (from reviewing online tutorials) */
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
 const symbol = '@#$%^&*()_+|}{[]></-=“';
-const min_Password_Length = 8;
-const max_Password_Length = 128;
+const minPasswordLength = 8;
+const maxPasswordLength = 128;
 
 var generateBtn = document.querySelector("#generate");
 
-// This function generates the password
+/* this function generates a password
+returns string | void */
 function generatePassword(){ 
 
-// First Prompt: Enter Password
-const length = prompt("Enter password length between 8 and 128:");
+// first user prompt
+const length = prompt("Start by choosing a number between 8 and 128. You'll be asked to choose how you want your password. Remember: the greater your variation, the stronger your password.");
 
-// Validation code
-const passwordLength = parseInt(length); // attempts to turn string into a number
-    if (
-      isNaN(passwordLength) || // compares numbers to numbers (and not strings)
-        passwordLength < min_Password_Length ||
-        passwordLength > max_Password_Length
+const passwordLength = parseInt(length); // <- attempts to turn a string into a number. 
+    if(
+      isNaN(passwordLength) || // <- if input is not a number OR if too short OR if too long
+        passwordLength < minPasswordLength ||
+        passwordLength > maxPasswordLength
     )
     {
-      return alert("Yo what the hell is this, try a password between 8 and 128 characters."); // returns the alert after calling function
+      return alert("Yo what the hell is this, try a number between 8 and 128."); 
+      // returns the alert after calling function
     }
 
-
-// User Prompts: Add to Input Pool // okay/cancel boolean
-const useUpperCase = confirm("Do you want uppercase, bro?");
-const useLowerCase = confirm("How about some lowercase letters, homes?");
-const useNumber = confirm("Now add some numbers!");
+//  user okay/cancel boolean
+const useUpperCase = confirm("Start by choosing an uppercase letter.");
+const useLowerCase = confirm("Now, choose whether you want lowercase letters.");
+const useNumber = confirm("Now add some numbers.");
 const useSymbol = confirm("Now add some sweet symbols, guy!");
 
-// These lines build the password from the pool generated from the above prompts
-let characters = ""; // changes to a string
-    if(useUpperCase === true){
-      characters += useUpperCase 
-    }
-// if (useUpperCase) characters += upperCase;
-if (useLowerCase) characters += lowerCase;
-if (useNumber) characters += number;
-if (useSymbol) characters += symbol;
+/* conditional gate sends user to beginning of process if all false
+if(!useUpperCase && !lowerCase && !useNumber && !useSymbol){ // must be 
+  alert("Come on, you gotta pick at least ONE ...")
+  return "Let's go again!"
+} */
 
-// Validation minimum 1 character type // conditional gates 
-    if (characters === ''){
+// these lines build the password from the pool generated from the above prompts
+var characters = ""; // changes to an empty string
+    if(useUpperCase){ // conditions to evaluate boolean
+      characters += upperCase; // based on user select, calls const value 
+  }
+    if(useLowerCase){
+      characters += lowerCase;
+  }
+    if(useNumber){
+      characters += number;
+  }
+    if(useSymbol){
+      characters += symbol; // appends const variables set out above
+  }
+  
+// conditional gate sends user to beginning of process if all false
+if(characters === ''){
+  alert("Come on, you gotta pick at least ONE ...")
+  return "Let's go again!"
+}
+
+// ^ can also be expressed as ...    
+    // if (useUpperCase) characters += upperCase;    
+    // if (useNumber) characters += number;
+    // if (useSymbol) characters += symbol;
+
+/* conditional gate sends user to beginning of process if all false
+    if(characters === ''){
       alert("Come on, you gotta pick at least ONE.")
       return;
-    }
+  } 
+  */
 
-    console.log(characters.length)
+ //   console.log(characters.length)
 
 // generating password from user input 
 let generatedPassword = "";
-for (let i=0; i < passwordLength; i++) { // user input determines .length loops X times
-const randomIndex = Math.floor(Math.random() * characters.length); // shows between 0 - 35 (if using letters + numbers only); math.floor rounds down, and math.random generates 0-1
+    for(let i=0; i < passwordLength; i++){ // user input determines .length loops X times, loop over available character set based on user input
+
+      const randomIndex = Math.floor(Math.random() * characters.length); 
+// ie. shows between 10 - 87 (if all characters); math.floor rounds down, and math.random generates 0-1
 generatedPassword += characters.charAt(randomIndex);
 }
 
