a36daea8ac6041cf2760b42a8908f3926de2722d
Daniel John <dcampo85@gmail.com>
2023-12-01 09:13:49 -0500

    js citation updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // By effectively captures the user's preferences, this code generates a password based on those preferences, and displays the password on a webpage when the user clicks a button
 
-// standard password constant variables that define character sets from which a password can be made. the min/max lengths define lengths for the generated password (found via reviewing online tutorials) 
+/* standard password constant variables that define character sets from which a password can be made. the min/max lengths define lengths for the generated password (found via reviewing online tutorials). the variable below assigns the "generate" HTML ID to the generateBtn object */
 const upCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowCase = 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
@@ -8,51 +8,47 @@ const symbol = '@#$%^&*()_+|}{[]></-=“';
 const minLength = 8;
 const maxLength = 128;
 
-// using querySelector this variable assigns HTML id to the generateBtn variable
 var generateBtn = document.querySelector("#generate");
 
-// this is the primary function of the generator: a prompt and  generates a password, returns string | void
+/* this is main function of the generator: the prompt generates a password, returns string | void. parseInt then tries to convert the string into a number, and compares the input with min/max password length using condition isNaN. if conditions are true (the input is not a number OR is too short, OR is too long) the user is sent back to the start of the process via the return alert */
 function generatePassword(){ 
-const length = prompt("Start by choosing a number between 8 and 128. You'll be asked to choose how you want your password. Remember: the greater your variation, the stronger your password."); // this is the first user prompt
-const passwordLength = parseInt(length); // <- attempts to turn a string into a number. 
+const length = prompt("Start by choosing a number between 8 and 128. You'll be asked to choose how you want your password. Remember: the greater your variation, the stronger your password."); 
+const passwordLength = parseInt(length); 
     if(
-      isNaN(passwordLength) || // <- if input is not a number OR if too short OR if too long
+      isNaN(passwordLength) ||
         passwordLength < minLength ||
         passwordLength > maxLength
     )
   {
     return alert("No, no, you have to use a number between 8 and 128. Remember?"); 
-      // returns the alert after calling function
   }
 
-//  user okay/cancel boolean
+/* the following consts use confirm dialogs for user inputs (ie. okay/cancel = true/false boolean). the variable characters initializes an empty string and builds the password based on user inputs using the 'if' statements to check which corresponding constant is true. if all inputs are false the final 'if' statement issues an alert and sends the user back to the start */
 const useUpperCase = confirm("Start by choosing an uppercase letter.");
 const useLowerCase = confirm("Now, choose whether you want lowercase letters.");
 const useNumber = confirm("Now add some numbers.");
 const useSymbol = confirm("Now add some symbols!");
 
-// these lines build the password from the pool generated from the above prompts
-var characters = ""; // changes to an empty string
-    if(useUpperCase){ // conditions to evaluate boolean
-      characters += upCase; // based on user select, calls const value 
-  }
+
+var characters = ""; 
+    if(useUpperCase){
+      characters += upCase; 
+    }
     if(useLowerCase){
       characters += lowCase;
-  }
+    }
     if(useNumber){
       characters += number;
-  }
+    }
     if(useSymbol){
       characters += symbol; // appends const variables set out above
-  }
-  
-// conditional gate sends user to beginning of process if all false
-if(characters === ''){
-  alert("Come on, you gotta pick at least ONE ...")
-  return "Let's go again!" 
-}
+    }
+    if(characters === ''){
+      alert("Come on, you gotta pick at least ONE ...")
+    return "Let's go again!" 
+    }
 
-let generatedPassword = ""; // generates password from user input
+let generatedPassword = ""; 
     for(let i=0; i < passwordLength; i++){ 
       // user input determines .length loops X times, looping over available character set based on user input
 
